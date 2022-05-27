5708988b710e826bfbe589358bb5bc940691682d
Anik Sen <aniksen.cuet09@gmail.com>
2022-05-28 00:00:15 +0600

    auth process.php

---
+++ authentication/process.php
@@ -0,0 +1,37 @@
+<?php
+
+$email = $_REQUEST['email'];
+$password = $_REQUEST['password'];
+$confirm_password = $_REQUEST['confirm_password'];
+
+
+
+
+    //echo $email;
+    //echo "sxsxs";
+	
+	// $stmt=$db->prepare("INSERT INTO tbl_user(username,
+	// 										 email, 
+	// 										 password)
+	// 									VALUES
+	// 									    (:uname,
+	// 										 :uemail,
+	// 										 :upassword)"); 
+	
+		
+	// $stmt->bindParam(":uname",$username);
+	// $stmt->bindParam(":uemail",$email);
+	// $stmt->bindParam(":upassword",$password);
+		 		
+	// if($stmt->execute())
+	// {
+	// 	echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button> 
+	// 				Register Successfully  
+	// 		 </div>';		
+	// }	
+	// else
+	// {
+	// 	echo  '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button> 
+	// 				Fail to Register  
+	// 		   </div>';		
+	// }
