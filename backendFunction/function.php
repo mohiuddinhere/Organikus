a4c606f20bd7763192e15fbb1f52646b9e6243b4
Zubayer <zubayer102019@gmail.com>
2022-06-02 14:36:20 +0600

    backend, authentication, designing some work done

--- backendFunction/function.php
+++ backendFunction/function.php
@@ -30,4 +30,7 @@ function passwordValidation($password){
     }
 }
 
+
+
+
 ?>
---
+++ customer-project/generate-project.php
@@ -0,0 +1,22 @@
+<?Php 
+
+include '../authentication/connection.php';
+
+$str = "SELECT * FROM operationtype";
+$result = mysqli_query($conn, $str);
+// $row = mysqli_num_rows($result);
+$arr = [];
+
+while($row = mysqli_fetch_array($result)){
+    $obj = [];
+    $id = $row['id'];
+    $name = $row['name'];
+    $obj['id'] = $id;
+    $obj['name'] = $name;
+    array_push($arr, $obj);
+}
+
+// print_r($arr);
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($arr);
+?>
